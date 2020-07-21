8a2098281fc81b80f9c5e6fd4f25a651ac3abc21
Vladimir Polishchuk <vnpolisch@yahoo.com>
2020-07-21 12:56:00 +0300

    first commit

--- project2/src/js/main.js
+++ project2/src/js/main.js
@@ -1,7 +1,10 @@
 import modals from './modules/modals.js';
+import sliders from './modules/sliders.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
-})
+    sliders('.feedback-slider-item','', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+});
